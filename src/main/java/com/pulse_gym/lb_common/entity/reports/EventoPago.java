7c88f239cf323d471bb94dca1a4e994a621799f7
Camilo Gallego <c.gallego3029@gmail.com>
2026-07-29 22:46:22 -0500

    fix: se arregla entidad

--- src/main/java/com/pulse_gym/lb_common/entity/reports/EventoPago.java
+++ src/main/java/com/pulse_gym/lb_common/entity/reports/EventoPago.java
@@ -4,10 +4,16 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Data
+@Table(name = "evento_pago")
 public class EventoPago {
     
     /**
