50cced338ba46602ecf12e2d60acd852f0f93b6e
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-29 20:32:30 -0500

    feat: agrego enums de ejercicio de estado

---
+++ src/main/java/com/pulse_gym/lb_common/enums/EnumEstadoEjecucionEjercicio.java
@@ -0,0 +1,13 @@
+package com.pulse_gym.lb_common.enums;
+
+public enum EnumEstadoEjecucionEjercicio {
+
+    /** Ejercicio completado totalmente */
+    COMPLETADO,
+
+    /** Ejercicio realizado parcialmente */
+    PARCIAL,
+
+    /** Ejercicio no realizado */
+    NO_REALIZADO
+}
