b2b9b9d8215b3aa2b85aae4c154b1d0a41163b9e
Camilo Gallego <c.gallego3029@gmail.com>
2026-07-29 21:44:49 -0500

    feat: creacion de 2 tablas inicales para el ms de reportes

---
+++ src/main/java/com/pulse_gym/lb_common/entity/reports/EventoPago.java
@@ -0,0 +1,50 @@
+package com.pulse_gym.lb_common.entity.reports;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class EventoPago {
+    
+    /**
+     * Identidficador unico del evento
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_evento_pago")
+    private Long idEventoPago;
+
+    /**
+     * Identidicador del socio
+     */
+    @Column(name = "socio_identificador")
+    private String socioIdentificador;
+
+    /**
+     * Monto pagado
+     */
+    @Column(name = "monto")
+    private BigDecimal monto;
+
+    /**
+     * Fecha de pago
+     */
+    @Column(name = "fecha_pago")
+    private LocalDateTime fechaPago;
+
+    /**
+     * Tipo de membresia
+     */
+    @Column(name = "tipo_membresia")
+    private String tipoMembresia;
+
+    /**
+     * Fecha de registro del sistema
+     */
+    @Column(name = "metodo_pago")
+    private String metodoPago;
+}
