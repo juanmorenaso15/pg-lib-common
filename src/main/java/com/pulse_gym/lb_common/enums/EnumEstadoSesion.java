2c38ffee85282d2ce6960cade910c4bd769bed66
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-29 20:30:52 -0500

    feat: agrego enum de sesion

---
+++ src/main/java/com/pulse_gym/lb_common/enums/EnumEstadoSesion.java
@@ -0,0 +1,13 @@
+package com.pulse_gym.lb_common.enums;
+
+public enum EnumEstadoSesion {
+
+    /** Sesión completada totalmente */
+    COMPLETADA,
+
+    /** Sesión realizada parcialmente */
+    PARCIAL,
+
+    /** Sesión no realizada */
+    NO_REALIZADA
+}
