cc0252433c68432be7c1f6e9b00fb56758a72bb5
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-07-30 11:21:17 -0500

    doc: agrego javadoc al dto

--- src/main/java/com/pulse_gym/lb_common/dto/EvolucionEjercicioDTO.java
+++ src/main/java/com/pulse_gym/lb_common/dto/EvolucionEjercicioDTO.java
@@ -6,13 +6,13 @@ import lombok.Data;
 
 @Data
 public class EvolucionEjercicioDTO {
-    
-    /** */
+
+    /** Nombre del ejercicio */
     private String nombreEjercicio;
-    
-    /** */
+
+    /** Progreso medido en peso o valor numérico */
     private BigDecimal progreso;
-    
-    /** */
+
+    /** Estado del progreso (PROGRESO, ESTANCADO, RETROCESO) */
     private String estado;
 }
