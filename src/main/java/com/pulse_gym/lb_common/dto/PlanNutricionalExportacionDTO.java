aec35968a73ca7a63df1dd1341630aabb0e5b199
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-08-03 07:22:26 -0500

    fix: corrijo DTOs de exportacion

--- src/main/java/com/pulse_gym/lb_common/dto/PlanNutricionalExportacionDTO.java
+++ src/main/java/com/pulse_gym/lb_common/dto/PlanNutricionalExportacionDTO.java
@@ -53,33 +53,3 @@ public class PlanNutricionalExportacionDTO {
 
 }
 
-/**
- * DTO para sugerencias de comida en exportación
- */
-@Data
-class SugerenciaComidaExportacionDTO {
-
-    /** Nombre de la comida */
-    private String nombre;
-
-    /** Descripción de la comida */
-    private String descripcion;
-
-    /** Calorías de la comida */
-    private Integer calorias;
-
-    /** Ingredientes de la comida */
-    private String ingredientes;
-
-    /** Preparación de la comida */
-    private String preparacion;
-
-    /** Gramos de proteína */
-    private Double proteinas;
-
-    /** Gramos de carbohidratos */
-    private Double carbohidratos;
-
-    /** Gramos de grasas */
-    private Double grasas;
-}
