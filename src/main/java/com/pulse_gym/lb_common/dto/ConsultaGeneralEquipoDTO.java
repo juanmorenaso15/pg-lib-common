2dff5a85bf7ce6ffcf6223d36e3bbe29bb1982e2
gallegohp <c.gallego3029@gmail.com>
2026-08-07 23:35:27 -0500

    feat: nuevo dto para la consulta de equipos general

---
+++ src/main/java/com/pulse_gym/lb_common/dto/ConsultaGeneralEquipoDTO.java
@@ -0,0 +1,16 @@
+package com.pulse_gym.lb_common.dto;
+
+import lombok.Data;
+
+@Data
+public class ConsultaGeneralEquipoDTO {
+
+    /**id del equipo */
+    private Long id;
+
+   /**nombre del equipo */
+    private String nombre;
+
+    /**marca del equipo */
+    private String estado;
+}
